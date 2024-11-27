d051dc624f610e058d8753a454239cb94440420e
BakytGaipov <bakyt.gaipov.kk@gmail.com>
2024-11-27 08:45:55 +0300

    added: install taiwind css

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello form deploy!</h1>
-        <p>It is not so dificalt to deploy the app</p>
+        <h1 className='text-3xl font-bold text-red-600'>
+          Hello form deploy!
+        </h1>
+        <p className='text-lg font-serif text-gray-600'>
+          It is not so dificalt to deploy the app
+        </p>
       </div>
       
     </>
