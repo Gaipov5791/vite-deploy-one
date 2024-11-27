3c38670e0c83f4ee3de7e7e6967f2f524e7a7f22
BakytGaipov <bakyt.gaipov.kk@gmail.com>
2024-11-27 08:35:53 +0300

    modified: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,9 @@ function App() {
     <>
       <div>
         <h1>Hello form deploy!</h1>
+        <p>It is not so dificalt to deploy the app</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
